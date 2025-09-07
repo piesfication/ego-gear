77a21762a4f081ec1e60a1c515c13c304a6cb136
Muhammad Rafi Sugianto <muhammad.rafi42@ui.ac.id>
2025-09-07 14:05:13 +0700

    Tugas Individu 2

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+def show_main(request):
+    context = {
+        'npm' : '2406357135',
+        'name': 'Muhammad Rafi Sugianto',
+        'class': 'PBP B'
+    }
+
+    return render(request, "main.html", context)
+
+# Create your views here.
