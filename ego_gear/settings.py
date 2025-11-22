ad28c71a8cd5eef040841eecd3da6d1959088f10
Muhammad Rafi Sugianto <muhammad.rafi42@ui.ac.id>
2025-11-22 19:58:36 +0700

    Cors allowed origins

--- ego_gear/settings.py
+++ ego_gear/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-rafi42-egogear.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-rafi42-egogear.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://muhammad-rafi42-egogear.pbp.cs.ui.ac.id"
@@ -47,7 +47,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,23 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "http://localhost:8080",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5000",
+    "http://127.0.0.1:8080",
 ]
 
 ROOT_URLCONF = 'ego_gear.urls'
--- ego_gear/urls.py
+++ ego_gear/urls.py
@@ -22,5 +22,7 @@ from django.urls import path, include
 ...
 
 urlpatterns = [
-    path('admin/', admin.site.urls), path('', include('main.urls')),
+    path('admin/', admin.site.urls), 
+    path('', include('main.urls')),
+    path('auth/', include('authentication.urls')),
 ]
